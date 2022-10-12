ff9234fc563b94aa671bacbc07d0c590a4dba171
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2022-10-12 22:34:06 +0600

    redirects page

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -5,7 +5,7 @@ import React from 'react';
 const ErrorPage = () => {
     return (
         <div>
-            <h2>Route is not avaiable</h2>
+            <h2 className='font-bold text-orange-400 mt-20' >Route is not avaiable</h2>
         </div>
     );
 };
